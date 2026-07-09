3ff9aecaedebf0a3465b97c6b52da00ef3bd1438
Subham Dey <sarmisthaad@gmail.com>
2026-07-09 20:37:48 +0530

    Minor changes I have made which was for Google Auth

--- src/middleware/userMiddleware.js
+++ src/middleware/userMiddleware.js
@@ -6,9 +6,9 @@ const userMiddleware = async (req, res, next) => {
   try {
     let token = req.cookies.token;
 
-    if (!token && req.headers.authorization?.startsWith("Bearer ")) {
-      token = req.headers.authorization.split(" ")[1];
-    }
+    // if (!token && req.headers.authorization?.startsWith("Bearer ")) {
+    //   token = req.headers.authorization.split(" ")[1];
+    // }
 
     if (!token) {
       return res.status(401).send("Token is not present.");
